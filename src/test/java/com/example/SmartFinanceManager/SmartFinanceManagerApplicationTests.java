0ea195b8f311e6739c908a020297ef68e32d9c2c
ron-neet <ronitshrestha17@gmail.com>
2025-09-06 14:10:22 +0545

    Second Commit

---
+++ src/test/java/com/example/SmartFinanceManager/SmartFinanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmartFinanceManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFinanceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
